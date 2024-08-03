a0f35da09b3667c341d4ab800c14502e82e3e31f
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2024-08-03 23:04:35 +0200

    Add bank verification via llava

--- ai_verifier/main.py
+++ ai_verifier/main.py
@@ -1,13 +1,55 @@
+import base64
+import os
+
 from fastapi import FastAPI
+import ollama
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class VerifierBank(BaseModel):
+    trade_contract_address: str
+    invoice: str
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/verify")
+async def verify_bank_transfer(
+        verify_bank_transfer_obj: VerifierBank
+):
+    image_data = base64.b64decode(verify_bank_transfer_obj.invoice)
+    file_path = "./invoice.jpg"
+
+    # Save base64 image into server for llava
+    with open(file_path, "wb") as image_file:
+        image_file.write(image_data)
+
+    # Ask llava model to extract information
+    res = ollama.chat(
+        model='llava',
+        messages=[{
+            "role": 'system',
+            "content": 'Check the image and find receiver IBAN and the amount. '
+                       'Then return message in this format { iban:string, amount:number, currency:string }',
+            "images": ['./invoice.jpg']
+        }]
+    )
+
+    # Delete the image file
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"Image {file_path} deleted successfully.")
+    else:
+        print(f"The file {file_path} does not exist.")
+
+    # TODO Call function in smart contract to check the transfer
+    return {"message": res["message"]["content"]}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    from os import getenv
+
+    host = getenv("HOST", "0.0.0.0")
+    port = int(getenv("PORT", "8080"))  # Default port is 8080 if not specified
+    uvicorn.run(app, host=host, port=port)
